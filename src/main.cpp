3279e4f558bf8bd123670b6b175d72e3173ccbe9
dylanhan99 <dylanhan99@gmail.com>
2025-07-16 22:25:53 +0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QObject>
+#include <QGridLayout>
+#include <QLineEdit>
+
+//class Counter : public QObject
+//{
+//    Q_OBJECT;
+//    Q_PROPERTY(int Value READ getValue WRITE setValue NOTIFY valueChanged);
+//
+//signals:
+//    inline void valueChanged(int val)
+//    {
+//        
+//    }
+//
+//private:
+//	int m_Value{ 0 };
+//};
 
 int main(int argc, char** argv)
 {
     QApplication app{ argc, argv };
+
+    QGridLayout layout{};
+    layout.addWidget(new QLineEdit{}, 0, 0, 1, 3);
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            int num = i * 3 + j;
+            layout.addWidget(new QPushButton{ QString{num} }, i, j);
+        }
+    }
 
     QPushButton button{};
     button.show();
