04c279cf09d24dffd35f23f5a2f9cd16f7da682d
dylanhan99 <dylanhan99@gmail.com>
2025-07-18 14:06:44 +0800

    updated cmake

--- cmake/ImportDependencies.cmake
+++ cmake/ImportDependencies.cmake
@@ -1,11 +1,6 @@
 include(FetchContent)
 
 macro(import_qt)
-    # Enable auto-MOC/UIC for Qt
-    set(CMAKE_AUTOMOC ON)
-    set(CMAKE_AUTOUIC ON)
-    set(CMAKE_AUTORCC ON)
-
     # Use find_package with vcpkg integration
     find_package(Qt5 REQUIRED COMPONENTS Core Gui Widgets)
 endmacro()
